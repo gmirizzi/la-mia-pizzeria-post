c8b8b1cbd9a8ab85e7b02eb6c696bc75b879b710
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-18 16:19:49 +0200

    validation

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -13,12 +13,16 @@ namespace la_mia_pizzeria_static.Models
 
         [Key]
         public int PizzaId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(25, ErrorMessage = "Il nome non può avere più di 25 caratteri")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
         [Column(TypeName = "text")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Url]
         public string ImgPath { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
         public string Price { get; set; }
     }
 }
